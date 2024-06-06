20153033b3181d53a0db32b5bf0808dbe4bf0330
Donna <dailydosesofdonuts@gmail.com>
2024-06-06 14:42:30 -0400

    Changed credentials info to .env

--- server/index.js
+++ server/index.js
@@ -6,6 +6,20 @@ import admin from 'firebase-admin';
 import * as dotenv from "dotenv";
 dotenv.config();
 
+const credentials = {
+    type: "service_account",
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-znb6i%40to-do-lists-7ac90.iam.gserviceaccount.com",
+    universe_domain: "googleapis.com"
+  }
+
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,11 +30,11 @@ import listRouter from "./src/routes/list-router.js";
 
 
 // set up firebase admin 
-const credentialsPath = path.join(__dirname, './credentials.json');
-const credentials = JSON.parse(
-    fs.readFileSync(credentialsPath)
+// const credentialsPath = path.join(__dirname, './credentials.json');
+// const credentials = JSON.parse(
+//     fs.readFileSync(credentialsPath)
     
-);
+// );
 
 admin.initializeApp({
     credential: admin.credential.cert(credentials)
