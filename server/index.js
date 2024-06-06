7499ae28926ee046092735c0d01e3c18f6035033
Donna <dailydosesofdonuts@gmail.com>
2024-06-06 18:48:45 -0400

    Fix3 vercel.json + index.js file path to dist

--- server/index.js
+++ server/index.js
@@ -45,10 +45,10 @@ admin.initializeApp({
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get(/^(?!\/api).+/, (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 const PORT = process.env.PORT || 8000;
